12ab62ead3b4a2f020d19929ef922bdda1fd345c
jiangpan <jiang.pan@outlook.com>
2020-03-09 11:15:37 +0800

    增加未处理异常

--- DBChmCreater/App.xaml.cs
+++ DBChmCreater/App.xaml.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace DBChmCreater
 {
@@ -14,13 +15,42 @@ namespace DBChmCreater
     /// </summary>
     public partial class App : Application
     {
+
+        public App()
+        {
+            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            string fileName = "app_" + DateTime.Now.ToString("yyyyMMdd") + ".log";
+            string path = System.IO.Path.Combine(AppContext.BaseDirectory, fileName);
+            File.AppendAllText(path, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} [{e.Exception.Message}] {"Application处理异常"} " + Environment.NewLine);
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            string fileName = "app_" + DateTime.Now.ToString("yyyyMMdd") + ".log";
+            string path = System.IO.Path.Combine(AppContext.BaseDirectory, fileName);
+            File.AppendAllText(path, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} [{e.ExceptionObject.ToString()}] {"Application处理异常"} " + Environment.NewLine);
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string fileName = "app_" + DateTime.Now.ToString("yyyyMMdd") + ".log";
+            string path = System.IO.Path.Combine(AppContext.BaseDirectory, fileName);
+            File.AppendAllText(path, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} [{e.Exception.Message}] {"Application处理异常"} " + Environment.NewLine);
+            e.Handled = true;
+        }
+
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             string fileName =  "app_" + DateTime.Now.ToString("yyyyMMdd") + ".log";
             string path = System.IO.Path.Combine(AppContext.BaseDirectory, fileName);
             File.AppendAllText(path, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} [{e.Exception.Message}] {"Application处理异常"} " + Environment.NewLine);
             e.Handled = true;
-
         }
     }
 }
