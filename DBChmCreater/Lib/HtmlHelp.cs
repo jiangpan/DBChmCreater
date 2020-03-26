9a88fbb16e5f8112b817fe8d1fba6334e16d6574
jiangpan <jiang.pan@outlook.com>
2020-03-26 11:12:02 +0800

    修改数据库目录 序号顺序

--- DBChmCreater/Lib/HtmlHelp.cs
+++ DBChmCreater/Lib/HtmlHelp.cs
@@ -84,7 +84,7 @@ namespace DBChmCreater.DB
             code.AppendLine("                    <td bgcolor=\"#FBFBFB\">");
             code.AppendLine("                        <table cellspacing=\"0\" cellpadding=\"5\" border=\"1\" width=\"100%\" bordercolorlight=\"#D7D7E5\" bordercolordark=\"#D3D8E0\">");
             code.AppendLine("                        <caption>");
-            code.AppendLine($"        <div class=\"styletab\">{dt.TableName}{(tableDesc.Length == 0 ? string.Empty : "  （" + tableDesc + "） ")}{(hasReturn ? "<a href ='../数据库表目录.html' style = 'float: left; margin-top: 6px;'>返回目录</a>" : string.Empty)}</div>");
+            code.AppendLine($"        <div class=\"styletab\">{dt.TableName}{( string.IsNullOrEmpty(tableDesc) ? string.Empty : "  （" + tableDesc + "） ")}{(hasReturn ? "<a href ='../数据库表目录.html' style = 'float: left; margin-top: 6px;'>返回目录</a>" : string.Empty)}</div>");
             //.FormatString(dt.TableName,
             //tableDesc.Length == 0 ? string.Empty : "  （" + tableDesc + "） ",
             //(hasReturn ? "<a href='../数据库表目录.html' style='float: right; margin-top: 6px;'>返回目录</a>" : string.Empty));
@@ -206,7 +206,7 @@ namespace DBChmCreater.DB
                 //一个domain
                 int count = domainitem.Count();
                 int i = 0;
-                var ere = domainitem.OrderBy(p => p.TableName);
+                var ere = domainitem.OrderBy(p => p.TableNo);
                 foreach (DataTableItem dritem in ere)
                 {
                     code.AppendLine("            <tr>");
@@ -249,7 +249,7 @@ namespace DBChmCreater.DB
                             }
                             else
                             {
-                                code.AppendLine($"            <td>{(dc.GetValue(dritem).ToString().Trim().Length > 0 ? dc.GetValue(dritem).ToString() : "&nbsp;")}</td>");
+                                code.AppendLine($"            <td>{( !string.IsNullOrWhiteSpace( dc.GetValue(dritem)?.ToString()) ? dc.GetValue(dritem)?.ToString() : "&nbsp;")}</td>");
                             }
                         }
 
